68f7290a190f3c51c2fc544cf2a073147f6531ee
NikVogri <55229210+NikVogri@users.noreply.github.com>
2020-12-23 20:02:03 +0100

    feat: display QR code

---
+++ components/QRCode.tsx
@@ -0,0 +1,27 @@
+import EthereumQRPlugin from "ethereum-qr-code";
+import { useEffect } from "react";
+import styles from "../styles/QRCode.module.scss";
+
+const QRCode = ({ address }) => {
+  useEffect(() => {
+    if (address) {
+      const qr = new EthereumQRPlugin();
+
+      console.log(qr);
+      qr.toCanvas(
+        {
+          to: address,
+          value: 1,
+        },
+        {
+          selector: `.${styles.qrcode}`,
+          scale: 60,
+        }
+      );
+    }
+  }, []);
+
+  return <div className={styles.qrcode} />;
+};
+
+export default QRCode;
