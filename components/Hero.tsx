3d6d6ba38c1aa6f3dcebd9de0ff9e8407894e7e3
NikVogri <55229210+NikVogri@users.noreply.github.com>
2020-12-23 19:57:11 +0100

    feat: hero

---
+++ components/Hero.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { useContext } from "react";
+import styles from "../styles/Hero.module.scss";
+import { LanguageContext } from "./partials/context/LanguageContext";
+import Navigation from "./partials/Navigation";
+
+const Hero = () => {
+  const { language } = useContext(LanguageContext);
+  return (
+    <div className={styles.hero}>
+      <div className={styles.backgroundHeader}>
+        <picture>
+          <source srcSet="/header@3x.jpg" media="(min-width: 1200px)" />
+          <source srcSet="/header@2x.jpg" media="(min-width: 820px)" />
+          <source srcSet="/header.jpg" media="(min-width: 10px)" />
+          <img src="/header.jpg" />
+        </picture>
+        <Link href={language == "it" ? "/it/donate" : "/far/donate"}>
+          <a className={styles.donateBtn}>Donate</a>
+        </Link>
+      </div>
+      <Navigation />
+    </div>
+  );
+};
+
+export default Hero;
