aa2f195003b0739a4f5e25915ff92bf27c24f56d
NikVogri <55229210+NikVogri@users.noreply.github.com>
2020-12-23 19:56:38 +0100

    feat: partials

---
+++ components/partials/Navigation.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { useContext } from "react";
+import { LanguageContext } from "./context/LanguageContext";
+
+import styles from "../../styles/Navigation.module.scss";
+import NavigationLinks from "../NavigationLinks";
+
+const Navigation = () => {
+  const { language } = useContext(LanguageContext);
+
+  return (
+    <nav className={styles.navigation}>
+      <Link href="/">
+        <a>
+          <img src="/atossa-logo.png" alt="Atossa" />
+        </a>
+      </Link>
+      <div>
+        <NavigationLinks />
+        <Link href={language == "far" ? `/far/donate` : `/it/donate`}>
+          <a className={styles.donateBtn}>Donate</a>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
