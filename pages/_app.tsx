a81e3dad1140e7279f32d6a26826ce7645d43ccc
NikVogri <55229210+NikVogri@users.noreply.github.com>
2020-12-23 19:59:59 +0100

    feat: redirect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import router from "next/dist/next-server/lib/router/router";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import LanguageProvider from "../components/partials/context/LanguageContext";
+import Layout from "../components/partials/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <LanguageProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </LanguageProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
