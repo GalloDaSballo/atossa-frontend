47dc23806e5d3ec85897146799719e8258ce0fc8
Entreprenerd <valeranijr@gmail.com>
2020-12-24 21:49:52 +0100

    fix smarter type

--- utils/formatImageUrl.ts
+++ utils/formatImageUrl.ts
@@ -3,16 +3,16 @@
  * Works for local and deployed strapis
  * @param {any} image 
  */
-export const fromImageToUrl = (image) => {
+export const fromImageToUrl = (image: string): string => {
   if (!image){
       return "/donate-header.jpg"
   }
 
-  if (image.url.indexOf("/") === 0){
-      return `${process.env.NEXT_PUBLIC_STRAPI_URL}${image.url}`
+  if (image.indexOf("/") === 0){
+      return `${process.env.NEXT_PUBLIC_STRAPI_URL}${image}`
   }
 
-  return image.url
+  return image
 }
 
 export const formatImageUrl = (imageUrl: string): string => {
