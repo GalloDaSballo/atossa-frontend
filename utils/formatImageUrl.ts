9a76d7b47347753b080a0997ae75eef59c5503c8
Entreprenerd <valeranijr@gmail.com>
2020-12-24 21:48:22 +0100

    fix image url for prod

--- utils/formatImageUrl.ts
+++ utils/formatImageUrl.ts
@@ -1,3 +1,20 @@
+/**
+ * Given an image return the Url
+ * Works for local and deployed strapis
+ * @param {any} image 
+ */
+export const fromImageToUrl = (image) => {
+  if (!image){
+      return "/donate-header.jpg"
+  }
+
+  if (image.url.indexOf("/") === 0){
+      return `${process.env.NEXT_PUBLIC_STRAPI_URL}${image.url}`
+  }
+
+  return image.url
+}
+
 export const formatImageUrl = (imageUrl: string): string => {
-  return `${process.env.NEXT_PUBLIC_STRAPI_URL}${imageUrl} `;
+  return fromImageToUrl(imageUrl)
 };
